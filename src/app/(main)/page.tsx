25aa134944c0b459d011e50f8a1200a0a563b55a
YuraSab <yrasabat@gmail.com>
2025-11-28 14:18:07 +0200

    feat: Setup core structure

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="">
+      <header className="">
+        header
+      </header>
+      <main className="">
+        main
+      </main>
+    </div>
+  );
+}
